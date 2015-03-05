52ed76e122cec47ef04cdef0b39580e414ba7d01
zhangxiaoya <tiyijushi@163.com>
2015-03-05 11:43:18 +0800

    advance search for eqm mp mpr

--- advancesearchdlg.cpp
+++ advancesearchdlg.cpp
@@ -6,6 +6,46 @@ AdvanceSearchDlg::AdvanceSearchDlg(QWidget *parent) :
     ui(new Ui::AdvanceSearchDlg)
 {
     ui->setupUi(this);
+    
+    if(!createConnection(db))
+    {
+        QMessageBox::warning(this,tr("数据库提示"),tr("不能链接数据库"),QMessageBox::Close);
+        return;
+    }
+    
+    tableNames.insert("EqmInfo","equipmentinfo");       // 装备信息表
+    tableNames.insert("AbmInfo","abrasivemarkinfo");    // 磨粒标注信息表
+    tableNames.insert("FegInfo","ferrographyinfo");     // 铁谱质谱信息表
+    tableNames.insert("FegPInfo","ferrographypicinfo"); // 铁谱图片信息表
+    tableNames.insert("MpInfo","movepartinfo");         // 动部件信息表
+    tableNames.insert("MprInfo","movepartrepairinfo");  //动部件维修信息表
+    tableNames.insert("OiaInfo","oilanalyzeinfo");      // 油样分析信息表
+    tableNames.insert("OisInfo","oilsampleinfo");       // 油样采样信息表
+    
+    // 装备信息表
+    _eqmInfoModel = new QSqlQueryModel;
+    ui->eqmTableView->setModel(_eqmInfoModel);
+    ui->eqmTableView->verticalHeader()->setVisible(false);
+    ui->eqmTableView->setSelectionBehavior(QTableView::SelectRows);
+    ui->eqmTableView->setSelectionMode(QTableView::SingleSelection);
+    ui->eqmTableView->setAlternatingRowColors(true);
+    
+    // 动部件信息表
+    _mpInfoModel = new QSqlQueryModel;
+    ui->mpTableView->setModel(_mpInfoModel);
+    ui->mpTableView->verticalHeader()->setVisible(false);
+    ui->mpTableView->setSelectionBehavior(QTableView::SelectRows);
+    ui->mpTableView->setSelectionMode(QTableView::SingleSelection);
+    ui->mpTableView->setAlternatingRowColors(true);
+    
+    // 动部件维修信息表
+    _mprInfoModel = new QSqlQueryModel;
+    ui->mprTableView->setModel(_mprInfoModel);
+    ui->mprTableView->verticalHeader()->setVisible(false);;
+    ui->mprTableView->setSelectionBehavior(QTableView::SelectRows);
+    ui->mprTableView->setSelectionMode(QTableView::SingleSelection);
+    ui->mprTableView->setAlternatingRowColors(true);
+    
 }
 
 AdvanceSearchDlg::~AdvanceSearchDlg()
@@ -15,14 +55,74 @@ AdvanceSearchDlg::~AdvanceSearchDlg()
 
 void AdvanceSearchDlg::on_queryBtn_clicked()
 {
-
+    
+    // 装备信息表
+    QString eqmTableName = tableNames.value("EqmInfo");
+    QString eqmSql = generateSql(_equipmentinfoCdtMap,_equipmentinfoCdtField,eqmTableName);
+    _eqmInfoModel->setQuery(eqmSql);
+    setModelHeaderData("EqmInfo");
+    qDebug()<<eqmSql;
+    
+    // 动部件信息表
+    QString mpTableName = tableNames.value("MpInfo");
+    QString mpSql = generateSql(_movepartinfoCdtMap,_movepartinfoCdtField,mpTableName);
+    _mpInfoModel->setQuery(mpSql);
+    setModelHeaderData("MpInfo");
+    qDebug()<<mpSql;
+    
+    // 动部件维修信息表
+    QString mprTableName = tableNames.value("MprInfo");
+    QString mprSql = generateSql(_movepartrepairinfoCdtMap,_movepartrepairinfoCdtField,mprTableName);
+    _mprInfoModel->setQuery(mprSql);
+    setModelHeaderData("MprInfo");
+    qDebug()<<mprSql;
+    
 }
 
-QString AdvanceSearchDlg::geneSql(QStringList conditionList, QString tableName)
+QString AdvanceSearchDlg::generateSql(QMap<QString, QString> conditionMap,QStringList conditionField, QString tableName)
 {
     QString sql = "select * from ";
     sql.append(tableName);
     sql.append(" ");
-//    if(conditionList.isEmpty())
+    if(conditionMap.isEmpty())
+    {
+        return sql;
+    }
+    else
+    {
+        sql.append("where ");
+        int fieldIndex  = 0;
+        QString fieldName = conditionField.at(fieldIndex);
+        QString fieldValue;
+        QMap<QString,QString>::iterator it;
+        for(it = conditionMap.begin();it != conditionMap.end()-1;++it)
+        {
+            fieldName = it.key();
+            fieldValue = it.value();
+            sql.append(fieldName);
+            sql.append("=");
+            sql.append(fieldValue);
+            sql.append(" and ");
+        }
+        fieldName = it.key();
+        fieldValue = it.value();
+        sql.append(fieldName);
+        sql.append("=");
+        sql.append(fieldValue);
+        return sql;
+    }
+}
 
+void AdvanceSearchDlg::setModelHeaderData(QString tablename)
+{
+    if(tablename == "EqmInfo")
+    {
+        _eqmInfoModel->setHeaderData(0,Qt::Horizontal,tr("序号"));
+        _eqmInfoModel->setHeaderData(1,Qt::Horizontal,tr("机号"));
+        _eqmInfoModel->setHeaderData(2,Qt::Horizontal,tr("机型"));
+        _eqmInfoModel->setHeaderData(3,Qt::Horizontal,tr("单位编号"));
+        _eqmInfoModel->setHeaderData(4,Qt::Horizontal,tr("飞行小时数"));
+        _eqmInfoModel->setHeaderData(5,Qt::Horizontal,tr("运行阶段"));
+        _eqmInfoModel->setHeaderData(6,Qt::Horizontal,tr("维修次数"));
+    }
 }
--- advancesearchdlg.h
+++ advancesearchdlg.h
@@ -2,8 +2,13 @@
 #define ADVANCESEARCHDLG_H
 
 #include <QDialog>
+#include <QSqlDatabase>
+#include <QSqlQueryModel>
 #include <QStringList>
 #include <QMap>
+#include <QDebug>
+
+#include "connection.h"
 
 namespace Ui {
 class AdvanceSearchDlg;
@@ -21,19 +26,33 @@ private slots:
      void on_queryBtn_clicked();
 
 private:
-     QString geneSql(QStringList conditionList,QString tableName);
+     QString generateSql(QMap<QString,QString> conditionMap,QStringList conditionField,QString tableName);
+     void setModelHeaderData(QString tablename);
 
 private:
     Ui::AdvanceSearchDlg *ui;
+    
+    QSqlDatabase db;
+    
+    QMap<QString,QString> tableNames;
+    QSqlQueryModel *_eqmInfoModel;
+    QSqlQueryModel *_abmInfoModel;
+    QSqlQueryModel *_fegInfoModel;
+    QSqlQueryModel *_fegpInfoModel;
+    QSqlQueryModel *_mpInfoModel;
+    QSqlQueryModel *_mprInfoModel;
+    QSqlQueryModel *_oiaInfoModel;
+    QSqlQueryModel *_oisInfoModel;
+    
 
-    QStringList _equipmentinfoCdtList;          // zhuang bei xin xi biao condition list
-    QStringList _abrasivemarkinfoCdtList;       // mo li biao ji xin xi biao condition list
-    QStringList _ferrographyinfoCdtList;        // tie pu zhi pu xin xi biao condition list
-    QStringList _ferrographypicinfoCdtList;     // tie pu tu pian cai ji xin xi biao condition list
-    QStringList _movepartinfoCdtList;           // dong bu jian xin xi biao condtion list
-    QStringList _movepartrepairinfoCdtList;     // dong bu jian wei xiu xin xi biao condition list
-    QStringList _oilanalyzeinfoCdtList;         // you yang fen xi xin xi biao condition list
-    QStringList _oilsampleinfoCdtList;          // you yang xin xi biao condition list
+    QStringList _equipmentinfoCdtField;          // zhuang bei xin xi biao condition Field
+    QStringList _abrasivemarkinfoCdtField;       // mo li biao ji xin xi biao condition Field
+    QStringList _ferrographyinfoCdtField;        // tie pu zhi pu xin xi biao condition Field
+    QStringList _ferrographypicinfoCdtField;     // tie pu tu pian cai ji xin xi biao condition Field
+    QStringList _movepartinfoCdtField;           // dong bu jian xin xi biao condtion Field
+    QStringList _movepartrepairinfoCdtField;     // dong bu jian wei xiu xin xi biao condition Field
+    QStringList _oilanalyzeinfoCdtField;         // you yang fen xi xin xi biao condition Field
+    QStringList _oilsampleinfoCdtField;          // you yang xin xi biao condition Field
 
     QMap<QString,QString> _equipmentinfoCdtMap;          // zhuang bei xin xi biao condition Map
     QMap<QString,QString> _abrasivemarkinfoCdtMap;       // mo li biao ji xin xi biao condition Map
